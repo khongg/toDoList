bd5eb849b6633ad2e041b098817f58998249db0b
Katharine Hong <meow24957@gmail.com>
2020-07-28 08:18:01 -0700

    Initial Commit

---
+++ toDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoList
+//
+//  Created by Sum Hong on 7/28/20.
+//  Copyright © 2020 Katharine Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
